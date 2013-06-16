f8693e268f471c0430265cbed0112c708b1fcb2c
Alex M <amessing@uvm.edu>
2013-06-15 20:43:37 -0400

    initial upload
    script and questions

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>CS 142 Assignment 4.1 Web Usability Testing</h1>
+<p>Site Link</p>
+<p>Videos of test</p>
+<p>Recommendations</p>
+<ol>
+  <li>ads</li>
+  <li>asdf</li>
+  <li>asdf</li>
+  <li>asdf</li>
+  <li>asdf</li>
+</ol>
+</body>
+</html>
